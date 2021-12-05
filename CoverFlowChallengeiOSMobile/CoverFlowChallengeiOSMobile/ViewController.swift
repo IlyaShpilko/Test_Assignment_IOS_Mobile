fa4e4d0a2c2a6dab706350608198c931bc405829
IlyaShpilko <61794412+IlyaShpilko@users.noreply.github.com>
2021-12-05 12:37:41 +0400

    Update: Added right switch

--- CoverFlowChallengeiOSMobile/CoverFlowChallengeiOSMobile/ViewController.swift
+++ CoverFlowChallengeiOSMobile/CoverFlowChallengeiOSMobile/ViewController.swift
@@ -17,13 +17,41 @@ class ViewController: UIViewController {
     
     var namesImages = ["gorillaz-plasticbeach", "ironmaiden-thefinalfrontier", "kidrock-bornfree", "kingsofleon-comearoundsunshine", "lilwayne-iamnotahumanbeing", "maroon5-handsallover", "mychemicalromance-dangerdays", "order of the black - black label society", "recovery-recovery", "screenshot", "taylorswift-speaknow", "thebandperry-thebandperry", "usher-raymondvraymond", "weezer-hurley"]
     
+    var startIndex = 0
+    
+    // MARK: - Life Cycle View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
+        startIndex = centerImage(arr: namesImages, element: namesImages.count / 2, right: nil)
+        print(startIndex)
         centerImage.image = UIImage(named: namesImages[namesImages.count / 2])
         
         transform3D(left: true, for: leftImage)
         transform3D(left: false, for: rightImage)
+        updateUIElements()
         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+
+        rightView.addGestureRecognizer(tap)
+        rightView.isUserInteractionEnabled = true
+        self.view.addSubview(rightView)
+    }
+
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        startIndex = centerImage(arr: namesImages, element: startIndex, right: true)
+        let elemenInArray = startIndex
+
+        leftImage.image = UIImage(named: namesImages[elemenInArray])
+        centerImage.image = UIImage(named: namesImages[elemenInArray + 1])
+        
+        if elemenInArray + 2 != namesImages.count {
+            rightImage.image = UIImage(named: namesImages[elemenInArray + 2])
+        } else {
+            updateUIElements()
+        }
+    }
+    
+    func updateUIElements() {
         if centerImage.image == UIImage(named: namesImages.first!) {
             leftView.isHidden = true
         } else if centerImage.image == UIImage(named: namesImages.last!) {
@@ -48,6 +76,23 @@ class ViewController: UIViewController {
             image.layer.transform = transform3D
 
         })
+    }
+    
+    // MARK: Helper Methods
+    func centerImage(arr: [String], element: Int, right: Bool?) -> Int {
+        if element == arr.count {
+            return element
+        }
+        
+        var result = element
+        
+        switch right {
+        case true: result += 1
+        case false: result -= 1
+        default: return result
+        }
+        
+        return result
     }
 }
 
